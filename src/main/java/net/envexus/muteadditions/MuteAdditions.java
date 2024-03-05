dd51a1663215c91e23caa816e3843929649c689a
envexus <joshua@envexus.net>
2024-03-05 04:36:20 +0000

    first commit

---
+++ src/main/java/net/envexus/muteadditions/MuteAdditions.java
@@ -0,0 +1,17 @@
+package net.envexus.muteadditions;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MuteAdditions extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
